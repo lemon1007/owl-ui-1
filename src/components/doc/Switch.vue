90ebeac2ddfed87759587deb0b59f32919d851d6
青砚 <lemon19961007@sina.com>
2022-09-26 12:26:04 +0800

    switch:优化代码

--- src/components/doc/Switch.vue
+++ src/components/doc/Switch.vue
@@ -5,14 +5,14 @@ import {ref} from 'vue';
 export default {
   components: {SwitchDemo},
   setup() {
-    const checked = ref(true);
-    return {checked};
+    const bool = ref(true);
+    return {bool};
   }
 };
 </script>
 
 <template>
-  <SwitchDemo v-model:value="checked"/>
+  <SwitchDemo v-model:value="bool"/>
 </template>
 
 <style lang="scss" scoped>
