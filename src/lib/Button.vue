ed0c226351a96350b21d568ea74d269cb0b4ac5c
青砚 <lemon19961007@sina.com>
2022-09-26 15:47:11 +0800

    button:添加基本样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -16,4 +16,29 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+$border-color: #3ab4e8;
+$color: #3ab4e8;
+.owl-button {
+  margin-top: 10px;
+  margin-right: 10px;
+  padding: 5px 10px;
+  border: 1px solid darkgray;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  border-radius: 10px;
+
+  &:hover {
+    border: 1px solid $border-color;
+    color: $color;
+  }
+
+  &:focus {
+    border: 1px solid $border-color;
+    color: $color;
+  }
+}
 </style>
