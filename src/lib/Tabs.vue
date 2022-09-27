9512f7162b73c3dbfb0b684c1461216f7fcd1dea
青砚 <lemon19961007@sina.com>
2022-09-27 08:59:52 +0800

    Tabs：添加判定子组件类型必须是Tab

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,6 +1,16 @@
 <script lang="ts">
+import Tab from './Tab.vue';
+
 export default {
-  setup(props: any, context: any) {}
+  setup(props: any, context: any) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag: any) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs的子组件必须是Tab');
+      }
+    });
+    return {defaults};
+  }
 };
 </script>
 
